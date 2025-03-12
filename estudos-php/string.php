9ad3bf7b7e4eb4fa6de5e5c9f16c23c2e450b0ea
Merieli Manzano <merieli.amanzano@gmail.com>
2025-03-12 07:48:39 -0300

    ate a aula 2 do modulo 3 de php - strings

--- estudos-php/string.php
+++ estudos-php/string.php
@@ -11,3 +11,22 @@ if (str_contains($frase, $nome)) {
 }
 
 echo PHP_EOL;
+
+
+// Em php uma string é como se fosse um array, porém não é um array. Por isso é possível acessar uma letra com índice
+$name = "Testesãáúíç";
+echo $name[0] . PHP_EOL; // M
+
+// A função strlen() retorna o tamanho da string, e esse tamanho é em bytes e não em quantidade de caracteres
+echo strlen($name) . PHP_EOL; // 7
+
+// Para retornar a quantidade de caracteres de uma string, é necessário usar a função mb_strlen()
+echo mb_strlen($name) . PHP_EOL; // 5
+
+// Converter para iso-8859-1
+$convertida = mb_convert_encoding($name, 'iso-8859-1', 'utf-8');
+echo $convertida . PHP_EOL; // ãáúíç
+
+// Converter para um formato
+$string = "um teste aqui";
+echo mb_convert_case($string, MB_CASE_TITLE) . PHP_EOL; // Testesãáúíç
--- estudos-php/user.php
+++ estudos-php/user.php
@@ -1,6 +1,6 @@
 <?php
 
-$email = 'merieli@dev.com.br';
+$email = 'meriéli@dev.com.br';
 
 echo substr($email, 0, 7) . PHP_EOL; // merieli
 echo substr($email, 8) . PHP_EOL;
@@ -17,3 +17,18 @@ echo substr($email, $positionOfArroba + 1) . PHP_EOL; // dev.com.br
 
 // --------------------
 // Atalho para buscar uma função na documentação do PHP: https://php.net/{nome_da_funcao}
+
+$usuario = substr($email, 0, $positionOfArroba);
+
+// strtoupper() - retorna a string com todas as letras maiúsculas, exceto caracteres acentuados e especiais que não estão na tabela ASCII
+echo strtoupper($usuario) . PHP_EOL; // MERIéLI
+// strtolower() - retorna a string com todas as letras minúsculas
+echo strtolower($usuario) . PHP_EOL; // meriéli
+
+// Extensão mbstring do PHP
+// mb_strlen() - retorna o tamanho da string, e esse tamanho é em quantidade de caracteres
+// mb_strtoupper() - retorna a string com todas as letras maiúsculas, exceto caracteres acentuados e especiais que não estão na tabela ASCII
+// mb_strtolower() - retorna a string com todas as letras minúsculas
+
+// Para visualizar as extensões PHP instaladas: `php -m`
+// Para verificar arquivos `ini` carregados que são arquivos de configuração: `php --ini`
